e2ad75019f1c28bb73706ecbbc713861ed0ac9fb
richarddwoods <richarddwoods@gmail.com>
2016-01-26 09:54:34 +1000

    #5 Improved error handling

--- bali-compiler-server/src/main/java/com/sealionsoftware/bali/compiler/server/CompileController.java
+++ bali-compiler-server/src/main/java/com/sealionsoftware/bali/compiler/server/CompileController.java
@@ -4,8 +4,12 @@ package com.sealionsoftware.bali.compiler.server;
 import com.sealionsoftware.bali.compiler.Interpreter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -42,13 +47,24 @@ public class CompileController {
         }));
     }
 
-    private <T> T runWithTimeout(Future<T> future) throws Exception{
+    private <T> T runWithTimeout(Future<T> future) throws Exception {
         try {
             return future.get(1, TimeUnit.SECONDS);
+        }  catch (ExecutionException exception){
+            Throwable cause = exception.getCause();
+            if (cause instanceof Exception){
+                throw (Exception) cause;
+            }
+            throw exception;
         } catch (TimeoutException timeout) {
             future.cancel(true);
-            throw timeout;
+            throw new RuntimeException("The submitted code timed out before completing", timeout);
         }
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(HttpMessageNotReadableException exception){
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
 }
