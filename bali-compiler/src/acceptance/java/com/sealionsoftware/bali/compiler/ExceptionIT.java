fdbb00048f53890193936ed7b6b453b4ea62f97c
richard <richarddwoods@gmail.com>
2017-08-02 07:36:44 +1000

    #22 - Added acceptance test for exception catching

--- bali-compiler/src/acceptance/java/com/sealionsoftware/bali/compiler/ExceptionIT.java
+++ bali-compiler/src/acceptance/java/com/sealionsoftware/bali/compiler/ExceptionIT.java
@@ -1,16 +1,20 @@
 package com.sealionsoftware.bali.compiler;
 
+import com.sealionsoftware.bali.compiler.execution.ReflectiveExecutor;
 import org.junit.Test;
 
 import static bali.text.Primitive.convert;
 import static com.sealionsoftware.Matchers.throwsException;
 import static com.sealionsoftware.bali.compiler.Matchers.withPayload;
+import static com.sealionsoftware.bali.compiler.Matchers.wrote;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ExceptionIT {
 
-    private Interpreter interpreter = new StandardInterpreter();
+    private ListTextBufferWriter console = new ListTextBufferWriter();
+    private Interpreter interpreter = new StandardInterpreter(null, null, null, new ReflectiveExecutor(console));
+
 
     @Test
     public void testThrowMessage() {
@@ -20,6 +24,20 @@ public class ExceptionIT {
         );
 
         assertThat(invocation, throwsException(withPayload(equalTo(convert("Ouch!")))));
+    }
+
+    @Test
+    public void testCatchException() {
+
+        interpreter.run(
+                "{" +
+                        "throw \"Ouch!\"" +
+                "} catch {" +
+                        "console << error + \" That hurt!\"" +
+                "}"
+        );
+
+        assertThat(console, wrote("Ouch! That hurt!"));
     }
 
 }
